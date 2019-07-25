dc629fe957ef2d2b392187962805e012bee463d4
Taehwan-Lab <52855867+Taehwan-Lab@users.noreply.github.com>
2019-07-25 11:53:40 +0900

    Add files via upload

--- 190725/keras15_lstm.py
+++ 190725/keras15_lstm.py
@@ -20,5 +20,16 @@ print("=====================")
 x_train = dataset[:, 0 : 4]
 y_train = dataset[:, 4]
 
+print(x_train.shape) # (6, 4)
+print(y_train.shape) # (6, )
+
+# x_train = np.reshape(x_train, (6, 4, 1))
+x_train = np.reshape(x_train, (len(a) - size + 1, 4, 1))
+
 print(x_train.shape)
-print(y_train.shape)
+
+x_test = np.array([[[11], [12], [13], [14]], [[12], [13], [14], [15]], [[13], [14], [15], [16]], [[14], [15], [16], [17]]])
+y_test = np.array([15, 16, 17, 18])
+
+print(x_test.shape)
+print(y_test.shape)
