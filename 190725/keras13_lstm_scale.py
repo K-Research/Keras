818bec15cf8b02616486f3214677c77822ddfe94
Taehwan-Lab <52855867+Taehwan-Lab@users.noreply.github.com>
2019-07-25 09:47:01 +0900

    Add files via upload

--- 190725/keras13_lstm_scale.py
+++ 190725/keras13_lstm_scale.py
@@ -3,8 +3,8 @@ from keras.models import Sequential
 from keras.layers import Dense, LSTM
 
 # 1. 데이터
-x = array([[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6], [5, 6, 7], [6, 7, 8], [7, 8, 9], [8, 9, 10], [9, 10, 11], [10, 11, 12]])
-y = array([4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+x = array([[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6], [5, 6, 7], [6, 7, 8], [7, 8, 9], [8, 9, 10], [9, 10, 11], [10, 11, 12], [20, 30, 40], [30, 40, 50], [40, 50, 60]])
+y = array([4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 50, 60, 70])
 
 print("x.shape : ", x.shape)
 print("y.shape : ", y.shape)
@@ -32,7 +32,7 @@ model.add(Dense(1))
 model.compile(optimizer = 'adam', loss = 'mse')
 model.fit(x, y, epochs = 1000)
 
-x_input = array([70, 80, 90])
+x_input = array([25, 35, 45])
 x_input = x_input.reshape(1, 3, 1)
 
 yhat = model.predict(x_input)
