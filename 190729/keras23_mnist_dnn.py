0f0c8c8a02451d1a65d6103c1f5d1e5babe09cc0
Taehwan-Lab <52855867+Taehwan-Lab@users.noreply.github.com>
2019-07-29 18:00:26 +0900

    Add files via upload

--- 190729/keras23_mnist_dnn.py
+++ 190729/keras23_mnist_dnn.py
@@ -18,7 +18,7 @@ from keras import models
 from keras import layers
 
 network = models.Sequential()
-network.add(layers.Dense(512, activation = 'relu', input_shape = (28 * 28, )))
+network.add(layers.Dense(512, activation = 'relu', input_shape = (28 * 28, ))) # 28 * 28 = 784
 network.add(layers.Dense(10, activation = 'softmax'))
 
 network.compile(optimizer = 'rmsprop', loss = 'categorical_crossentropy', metrics = ['accuracy'])
