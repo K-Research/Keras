403e0db90bf24c9340f7fba40b01f89eccd74906
Taehwan-Lab <52855867+Taehwan-Lab@users.noreply.github.com>
2019-07-29 14:35:35 +0900

    Add files via upload

--- 190729/keras21_cnn1.py
+++ 190729/keras21_cnn1.py
@@ -7,9 +7,10 @@ from keras.layers import Conv2D, MaxPooling2D
 model = Sequential()
 model.add
 # model.add(Conv2D(filter_size, kernel_size, input_shape = (28, 28, 1)))
-model.add(Conv2D(7, (2, 2), padding = 'same', input_shape = (5, 5, 1)))
+# model.add(Conv2D(7, (2, 2), padding = 'same', input_shape = (5, 5, 1)))
+model.add(Conv2D(7, (2, 2), padding = 'same', input_shape = (10, 10, 1)))
 model.add(Conv2D(16, (2, 2)))
-model.add(MaxPooling2D(2, 2))
+model.add(MaxPooling2D(3, 3))
 model.add(Conv2D(8, (2, 2)))
 
 model.summary()
