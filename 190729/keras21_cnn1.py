635057e5c2514e60cbb3402243287f5ab5fc553b
Taehwan-Lab <52855867+Taehwan-Lab@users.noreply.github.com>
2019-07-29 14:10:02 +0900

    Add files via upload

--- 190729/keras21_cnn1.py
+++ 190729/keras21_cnn1.py
@@ -6,7 +6,8 @@ kernel_size = (3, 3)
 from keras.layers import Conv2D
 model = Sequential()
 model.add
-model.add(Conv2D(filter_size, kernel_size, input_shape = (28, 28, 1)))
+# model.add(Conv2D(filter_size, kernel_size, input_shape = (28, 28, 1)))
+model.add(Conv2D(7, (2, 2), input_shape = (5, 5, 1)))
 model.add(Conv2D(16, (3, 3)))
 model.add(Conv2D(8, (2, 2)))
 
