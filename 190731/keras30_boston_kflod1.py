bcb4b489e5bce2c0b73fe3659eb029d1630967bf
Taehwan-Lab <52855867+Taehwan-Lab@users.noreply.github.com>
2019-07-31 14:55:11 +0900

    Add files via upload

--- 190731/keras30_boston_kflod1.py
+++ 190731/keras30_boston_kflod1.py
@@ -7,7 +7,51 @@ from keras.layers import Dense, LSTM, Dropout
 print(train_data.shape)
 print(test_data.shape)
 
-'''
-mean = train_data.mean(axis = 8)
+mean = train_data.mean(axis = 0)
 train_data -= mean
-'''
+std = train_data.std(axis = 0)
+
+test_data -= mean
+test_data /= std
+
+from keras import models
+from keras import layers
+
+def build_model():
+    # 동일한 모델을 여러 번 생성할 것이므로 함수를 만들어 사용합니다
+    model = models.Sequential()
+    model.add(layers.Dense(64, activation = 'relu', input_shape = (train_data.shape[1], )))
+    model.add(layers.Dense(64, activation = 'relu'))
+    model.add(layers.Dense(1))
+    model.compile(optimizer = 'rmsprop', loss = 'mse', metrics = ['mae'])
+    return model
+
+import numpy as np
+
+k = 4
+num_val_samples = len(train_data) // k
+num_epochs = 100
+all_scores = []
+
+for i in range(k):
+    print('처리중인 폴드 #', i)
+    # 검증 데이터 준비  k번째 분할
+    val_data = train_data[i * num_val_samples : (i + 1) * num_val_samples]
+    val_targets = train_targets[i * num_val_samples : (i + 1) * num_val_samples]
+
+    # 훈련 데이터 준비 : 다른 분할 전체
+    partial_train_data = np.concatenate([train_data[ : i * num_val_samples], train_data[(i + 1) * num_val_samples : ]], axis = 0)
+    partial_train_targets = np.concatenate([train_targets[ : i * num_val_samples], train_targets[( i + 1) * num_val_samples : ]], axis = 0)
+
+    # 케라스 모델 구성 (컴파일 포함)
+    model = build_model()
+
+    # 모델 훈련 ( verbose = 0 이므로 훈련 과정이 출력되지 않습니다)
+    model.fit(partial_train_data, partial_train_targets, epochs = num_epochs, batch_size = 1, verbose = 1) # verbose = 0
+
+    # 검증 세트로 모델 평가
+    val_mse, val_mae = model.evaluate(val_data, val_targets, verbose = 0)
+    all_scores.append(val_mae)
+
+    print(all_scores)
+    print(np.mean(all_scores))
