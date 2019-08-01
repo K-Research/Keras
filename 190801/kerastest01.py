e2467e49579c3ee720272317619be71d116c6061
Taehwan-Lab <52855867+Taehwan-Lab@users.noreply.github.com>
2019-08-01 16:55:52 +0900

    Add files via upload

--- 190801/kerastest01.py
+++ 190801/kerastest01.py
@@ -5,6 +5,7 @@ from keras.layers.recurrent import LSTM
 from keras.models import Sequential
 import numpy
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import time
 
 dataset_market = pandas.read_csv('kospi.csv', usecols = [1])
 dataset_high = pandas.read_csv('kospi.csv', usecols = [2])
@@ -13,16 +14,11 @@ dataset_closing = pandas.read_csv('kospi.csv', usecols = [4])
 dataset_volume = pandas.read_csv('kospi.csv', usecols = [5])
 dataset_exchange = pandas.read_csv('kospi.csv', usecols = [6])
 
-x_train_market = dataset_market[0 : 480]
-x_test_market = dataset_market[480 : 600]
-x_train_high = dataset_high[0 : 480]
-x_test_high = dataset_high[480 : 600]
-x_train_low = dataset_low[0 : 480]
-x_test_low = dataset_low[480 : 600]
-x_train_volume = dataset_volume[0 : 480]
-x_test_volume = dataset_volume[480 : 600]
-x_train_exchange = dataset_exchange[0 : 480]
-x_test_exchange = dataset_exchange[480 : 600]
+x_train_market = dataset_market
+x_train_high = dataset_high
+x_train_low = dataset_low
+x_train_volume = dataset_volume
+x_train_exchange = dataset_exchange
 
 x_train_market = x_train_market.values.reshape(x_train_market.shape[0], x_train_market.shape[1])
 x_train_high = x_train_high.values.reshape(x_train_high.shape[0], x_train_high.shape[1])
@@ -41,19 +37,32 @@ x_train_exchange = scaler.transform(x_train_exchange)
 
 x_train = numpy.concatenate((x_train_market, x_train_high, x_train_low, x_train_volume, x_train_exchange), axis = 1)
 
-x_train = x_train.reshape(480, 5, 1)
+x_train = x_train.reshape(599, 5, 1)
 
-y_train = dataset_closing[0 : 480]
-y_test = dataset_closing[480 : 600]
+y_train = dataset_closing
 
 x_input = array([2026.10, 2032.23, 2009.33, 284936, 1187.80])
 x_input = x_input.reshape(1, 5, 1)
 
+#2단계: 모델 생성
 model = Sequential()
+
 model.add(LSTM(input_dim = 1, output_dim = 50, return_sequences = True))
+model.add(Dropout(0.2))
 model.add(LSTM(100, return_sequences = False))
+model.add(Dropout(0.2))
 model.add(Dense(output_dim = 1))
 model.add(Activation('linear'))
+
+start = time.time()
+model.compile(loss='mse', optimizer='rmsprop')
+print('Compilation time : ', time.time() - start)
+
+# model = Sequential()
+# model.add(LSTM(input_dim = 1, output_dim = 50, return_sequences = True))
+# model.add(LSTM(100, return_sequences = False))
+# model.add(Dense(output_dim = 1))
+# model.add(Activation('linear'))
 
 model.compile(loss = 'mse', optimizer = 'rmsprop')
 
