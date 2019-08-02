2bfc5b1bd75c58323c15bcd601865896ec85396f
Taehwan-Lab <52855867+Taehwan-Lab@users.noreply.github.com>
2019-08-02 16:08:08 +0900

    Add files via upload

--- 190802/kospi.py
+++ 190802/kospi.py
@@ -5,6 +5,7 @@ from keras.layers.recurrent import LSTM
 from keras.models import Sequential
 import numpy
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 kospi = pandas.read_csv('kospi.csv')
 dataset_closing_price = array(kospi["Closing price"])
@@ -24,25 +25,23 @@ dataset = array(dataset)
 x = dataset[:, 0 : 2]
 y = dataset[:, 2 : ]
 
+scaler = MinMaxScaler()
+scaler.fit(x)
+x = scaler.transform(x)
+
 x = x.reshape(596, 2, 1)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 66, test_size = 0.0001)
 
 model = Sequential()
 
-model.add(LSTM(50, input_shape = (2, 1), return_sequences = True))
-model.add(LSTM(100, return_sequences = False))
-model.add(Dense(64))
-model.add(Dense(32))
-model.add(Dense(16))
-model.add(Dense(8))
-model.add(Dense(4))
+model.add(LSTM(50, input_shape = (2, 1), return_sequences = False))
 model.add(Dense(2))
 model.add(Activation('linear'))
 
 model.compile(loss = 'mse', optimizer = 'rmsprop')
 
-model.fit(x_train, y_train, batch_size = 1, nb_epoch = 100, validation_split = 0.05)
+model.fit(x_train, y_train, batch_size = 1, nb_epoch = 1000, validation_split = 0.05)
 
 y_predict = model.predict(x_test)
 print(y_predict)
