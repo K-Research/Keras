bb40d0d6203970f3c0e8ed608ca21d5761d89c83
Taehwan-Lab <52855867+Taehwan-Lab@users.noreply.github.com>
2019-08-02 17:05:34 +0900

    Add files via upload

--- 190802/keras35_lstm_mlp1.py
+++ 190802/keras35_lstm_mlp1.py
@@ -25,9 +25,7 @@ y = dataset[:, 4 : ]
 
 x = np.reshape(x, (len(array) - size + 1, 4, 1))
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 66, test_size = 0.01)
-
-x_predict = np.array([[[111], [112], [113], [114]]])
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 66, test_size = 0.2)
 
 model = Sequential()
 
@@ -43,10 +41,9 @@ model.fit(x_train, y_train, epochs = 100, batch_size = 1, verbose = 1, callbacks
 
 loss, acc = model.evaluate(x_test, y_test)
 
-# y_predict = model.predict(x_test)
-y_predict = model.predict(x_predict)
+y_predict = model.predict(x_test)
 
-print('y_predict(x_test) : \n', y_predict)
+print('y_predict(x_test) : \n', y_test)
 
 def RMSE(y_test, y_predict):
     return np.sqrt(mean_squared_error(y_test, y_predict))
